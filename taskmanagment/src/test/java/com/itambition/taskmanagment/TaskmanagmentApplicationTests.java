f472449a842d9e19fe84b2c92f762805b1aedc3b
Ayman El Shayeb | Ejada Systems <aelshayeb@ejada.com>
2025-07-30 12:18:18 +0300

    first version

---
+++ taskmanagment/src/test/java/com/itambition/taskmanagment/TaskmanagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itambition.taskmanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
